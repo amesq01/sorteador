70e41f4ed0508b5a6e1518cdc819da0b8a9dfa56
Adailton Mesquita <amesq1@hotmail.com>
2023-03-31 13:25:09 -0300

    add environment variables on project

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -4,13 +4,13 @@ import * as firebaseAuth from 'firebase/auth';
 import { browserLocalPersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECTD_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SEND_ID,
-  appId: import.meta.env.VITE_APP_ID,
-  measurementId: import.meta.env.VITE_MEASUREMENT_ID,
+  apiKey: process.env.VITE_API_KEY,
+  authDomain: process.env.VITE_AUTH_DOMAIN,
+  projectId: process.env.VITE_PROJECTD_ID,
+  storageBucket: process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_MESSAGING_SEND_ID,
+  appId: process.env.VITE_APP_ID,
+  measurementId: process.env.VITE_MEASUREMENT_ID,
 
 };
 const app = firebase.initializeApp(firebaseConfig);
